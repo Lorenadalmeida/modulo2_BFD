396ef5ae3c4f5cc0748b65aa08e62e449ca7717e
Lorenadalmeida <llorenaliv07@gmail.com>
2025-09-18 23:47:07 +0000

    .

---
+++ poo_heranca.py
@@ -0,0 +1,12 @@
+class Usuario:
+     def _init_(self, nome):
+        self.nome() 
+        def _init_(self, gmail):
+            self.gmail()
+
+class cliente:
+    def __init__(self):
+        pass
+
+
+
